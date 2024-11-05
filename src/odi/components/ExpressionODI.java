30c4e61049cf4ed2cea0b3a1d2fa5748dff5af77
rzagni <renzo@zagni.com>
2024-11-05 12:39:10 -0800

    Update ExpressionODI.java

--- src/odi/components/ExpressionODI.java
+++ src/odi/components/ExpressionODI.java
@@ -1,3 +1,20 @@
+/**
+ * ExpressionODI - This utility class is responsible for creating and configuring an ODI (Oracle Data Integrator)
+ * expression component within a mapping. It maps transformation fields from an Informatica expression transformation
+ * to an ODI expression component, including setting field names, expressions, and precision values.
+ * 
+ * <p>
+ * Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * You may use, modify, and share this code for non-commercial purposes, provided you give appropriate
+ * credit, indicate if changes were made, and distribute any modified work under the same license.
+ * </p>
+ *
+ * @author Renzo Zagni
+ * @license Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+ * @see <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons License</a>
+ */
+
+
 package odi.components;
 
 import infa.map.TransformField;
