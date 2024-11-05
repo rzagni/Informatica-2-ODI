eb050d381dfc383b17b32073f2226270f6be168b
rzagni <renzo@zagni.com>
2024-11-05 12:48:42 -0800

    Update ODIComponentType.java

--- src/odi/map/ODIComponentType.java
+++ src/odi/map/ODIComponentType.java
@@ -1,3 +1,20 @@
+/**
+ * ODIComponentType - This utility class defines constants representing various ODI (Oracle Data Integrator) 
+ * component types. Each constant corresponds to a specific component type name used in ODI mappings, 
+ * enabling easier reference and standardization within code.
+ * 
+ * <p>
+ * Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * You may use, modify, and share this code for non-commercial purposes, provided you give appropriate
+ * credit, indicate if changes were made, and distribute any modified work under the same license.
+ * </p>
+ *
+ * @author Renzo Zagni
+ * @license Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+ * @see <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons License</a>
+ */
+
+
 package odi.map;
 
 import oracle.odi.domain.mapping.component.*;
