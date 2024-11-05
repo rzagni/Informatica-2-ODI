9341d53296b423332691b3e6ee1bd4a97f095dd0
rzagni <renzo@zagni.com>
2024-11-05 12:40:17 -0800

    Update MappingODI.java

--- src/odi/components/MappingODI.java
+++ src/odi/components/MappingODI.java
@@ -1,3 +1,20 @@
+/**
+ * MappingODI - This utility class is responsible for creating and configuring an ODI (Oracle Data Integrator)
+ * mapping component within a specified folder. It maps information from an Informatica mapping object to an ODI
+ * mapping, setting properties such as description and business name. If the mapping already exists, it resets its
+ * components before reconfiguring.
+ * 
+ * <p>
+ * Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * You may use, modify, and share this code for non-commercial purposes, provided you give appropriate
+ * credit, indicate if changes were made, and distribute any modified work under the same license.
+ * </p>
+ *
+ * @author Renzo Zagni
+ * @license Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+ * @see <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons License</a>
+ */
+
 package odi.components;
 
 import infa.map.InfMapping;
