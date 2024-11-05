907cf4eb02a5385134a542bf03a6e192973694fd
rzagni <renzo@zagni.com>
2024-11-05 12:41:42 -0800

    Update ModelFolderTargetODI.java

--- src/odi/components/ModelFolderTargetODI.java
+++ src/odi/components/ModelFolderTargetODI.java
@@ -1,3 +1,19 @@
+/**
+ * ModelFolderTargetODI - This utility class manages the creation and configuration of an ODI (Oracle Data Integrator)
+ * target model within a specified folder. It maps target information from an ODI project to an ODI model, setting
+ * the model name and code based on project details for target mappings.
+ * 
+ * <p>
+ * Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * You may use, modify, and share this code for non-commercial purposes, provided you give appropriate
+ * credit, indicate if changes were made, and distribute any modified work under the same license.
+ * </p>
+ *
+ * @author Renzo Zagni
+ * @license Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+ * @see <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons License</a>
+ */
+
 package odi.components;
 
 import infa2odi.commons.MigrationODI;
