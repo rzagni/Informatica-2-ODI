56d18a2a1e689140e52bcd4df5a450c8d38fb902
rzagni <renzo@zagni.com>
2024-11-05 12:42:57 -0800

    Update ProjectFolderODI.java

--- src/odi/components/ProjectFolderODI.java
+++ src/odi/components/ProjectFolderODI.java
@@ -1,3 +1,19 @@
+/**
+ * ProjectFolderODI - This utility class manages the creation and configuration of an ODI (Oracle Data Integrator)
+ * project folder for a specified project. It checks if a folder already exists within the project and creates 
+ * a new one if necessary.
+ * 
+ * <p>
+ * Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * You may use, modify, and share this code for non-commercial purposes, provided you give appropriate
+ * credit, indicate if changes were made, and distribute any modified work under the same license.
+ * </p>
+ *
+ * @author Renzo Zagni
+ * @license Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+ * @see <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons License</a>
+ */
+
 package odi.components;
 
 import infa2odi.commons.MigrationODI;
