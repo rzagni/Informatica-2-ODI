583a5727a39ac652f209f1bd4356c41be43455c8
rzagni <renzo@zagni.com>
2024-11-05 12:42:21 -0800

    Update ModelODI.java

--- src/odi/components/ModelODI.java
+++ src/odi/components/ModelODI.java
@@ -1,3 +1,19 @@
+/**
+ * ModelODI - This utility class handles the creation and configuration of an ODI (Oracle Data Integrator)
+ * model folder for a specified project. It checks if a model folder already exists for the project, 
+ * creating a new one if necessary.
+ * 
+ * <p>
+ * Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * You may use, modify, and share this code for non-commercial purposes, provided you give appropriate
+ * credit, indicate if changes were made, and distribute any modified work under the same license.
+ * </p>
+ *
+ * @author Renzo Zagni
+ * @license Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+ * @see <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons License</a>
+ */
+
 package odi.components;
 
 import infa2odi.commons.MigrationODI;
